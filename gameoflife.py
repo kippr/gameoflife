4b46ef4a0b9efe162d2bd1812040e3b2b0c2dfd9
Kris Powell <kris.powell@kkr.com>
2020-07-01 21:49:36 +0100

    wip

--- gameoflife.py
+++ gameoflife.py
@@ -5,7 +5,7 @@ from expecter import expect
 class GameOfLife(object):
     def __init__(self, board):
         self.board = board
-        self.size = math.sqrt(len(board))
+        self.size = int(math.sqrt(len(board)))
         assert set(board).issubset(set('.#')), "Pass board as string of . (dead) and # (alive)"
         if '#' in board:
             self.board = [[1,0,0], [0,1,0], [0,0,1]]
