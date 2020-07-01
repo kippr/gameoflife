39558d7fa1c597ae25e3f371132d2824f6f8667f
Kris Powell <kris.powell@kkr.com>
2020-07-01 21:59:07 +0100

    wip

--- gameoflife.py
+++ gameoflife.py
@@ -8,8 +8,8 @@ class GameOfLife(object):
         assert size * size == len(board), 'Pass a square board please'
         assert set(board).issubset(set('.#')), "Pass board as string of . (dead) and # (alive)"
         self.board = [[int(board[(y * size) + x] == '#')
-                     for x in range(size)]
-                     for y in range(size)]
+                      for x in range(size)]
+                      for y in range(size)]
 
     @property
     def size(self):
