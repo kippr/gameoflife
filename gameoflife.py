2289ead15c54808705a10ad8d89ca97f7eee6d5b
Kris Powell <kris.powell@kkr.com>
2020-07-01 21:43:30 +0100

    wip

--- gameoflife.py
+++ gameoflife.py
@@ -6,7 +6,7 @@ class GameOfLife(object):
     def __init__(self, board):
         self.board = board
         self.size = math.sqrt(len(board))
-        assert set(board) == set('.')
+        assert set(board).issubset(set('.#')), "Pass board as string of . (dead) and # (alive)"
 
 
 def board(board):
